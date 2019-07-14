e534c09d1c049823afc9539bb6aa00af4bbddc4c
Michal Walczynski <mwalczynski@pgs-soft.com>
2019-07-14 14:05:09 +0200

    AddTransient -> AddScoped

--- CustomModelBinder/Startup.cs
+++ CustomModelBinder/Startup.cs
@@ -21,7 +21,7 @@ namespace CustomModelBinder
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddTransient<IProductsProvider, FakeProductsProvider>();
+            services.AddScoped<IProductsProvider, FakeProductsProvider>();
 
             services.AddMvc(options =>
             {
